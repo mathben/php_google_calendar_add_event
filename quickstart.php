f5087738009111bb623f4d207fef20dccd784fdb
Mathieu Benoit <mathben963@gmail.com>
2017-04-22 13:41:58 -0400

    calendar write and read - add event for test

--- quickstart.php
+++ quickstart.php
@@ -8,7 +8,7 @@ define('CLIENT_SECRET_PATH', __DIR__ . '/secret/client_secret.json');
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/calendar-php-quickstart.json
 define('SCOPES', implode(' ', array(
-  Google_Service_Calendar::CALENDAR_READONLY)
+  Google_Service_Calendar::CALENDAR)
 ));
 
 if (php_sapi_name() != 'cli') {
@@ -75,24 +75,61 @@ $client = getClient();
 $service = new Google_Service_Calendar($client);
 
 // Print the next 10 events on the user's calendar.
-$calendarId = 'primary';
-$optParams = array(
-  'maxResults' => 10,
-  'orderBy' => 'startTime',
-  'singleEvents' => TRUE,
-  'timeMin' => date('c'),
-);
-$results = $service->events->listEvents($calendarId, $optParams);
+//$calendarId = 'primary';
+//$optParams = array(
+//  'maxResults' => 10,
+//  'orderBy' => 'startTime',
+//  'singleEvents' => TRUE,
+//  'timeMin' => date('c'),
+//);
+//$results = $service->events->listEvents($calendarId, $optParams);
+//
+//if (count($results->getItems()) == 0) {
+//  print "No upcoming events found.\n";
+//} else {
+//  print "Upcoming events:\n";
+//  foreach ($results->getItems() as $event) {
+//    $start = $event->start->dateTime;
+//    if (empty($start)) {
+//      $start = $event->start->date;
+//    }
+//    printf("%s (%s)\n", $event->getSummary(), $start);
+//  }
+//}
 
-if (count($results->getItems()) == 0) {
-  print "No upcoming events found.\n";
-} else {
-  print "Upcoming events:\n";
-  foreach ($results->getItems() as $event) {
-    $start = $event->start->dateTime;
-    if (empty($start)) {
-      $start = $event->start->date;
-    }
-    printf("%s (%s)\n", $event->getSummary(), $start);
-  }
-}
+// Refer to the PHP quickstart on how to setup the environment:
+// https://developers.google.com/google-apps/calendar/quickstart/php
+// Change the scope to Google_Service_Calendar::CALENDAR and delete any stored
+// credentials.
+
+$event = new Google_Service_Calendar_Event(array(
+  'summary' => 'Test Jean Roger',
+  'location' => '4141 Pierre-de Coubertin Ave, Montreal, QC H1V 3N7',
+  'description' => 'Un tour au stade olympique avec pleins d\'enfant heureux!',
+  'start' => array(
+    'dateTime' => '2017-04-23T09:00:00-04:00',
+    'timeZone' => 'America/Los_Angeles',
+  ),
+  'end' => array(
+    'dateTime' => '2017-04-23T15:00:00-04:00',
+    'timeZone' => 'America/Los_Angeles',
+  ),
+//  'recurrence' => array(
+//    'RRULE:FREQ=DAILY;COUNT=2'
+//  ),
+//  'attendees' => array(
+//    array('email' => 'lpage@example.com'),
+//    array('email' => 'sbrin@example.com'),
+//  ),
+//  'reminders' => array(
+//    'useDefault' => FALSE,
+//    'overrides' => array(
+//      array('method' => 'email', 'minutes' => 24 * 60),
+//      array('method' => 'popup', 'minutes' => 10),
+//    ),
+//  ),
+));
+
+$calendarId = 'primary';
+$event = $service->events->insert($calendarId, $event);
+printf("Event created: %s\n", $event->htmlLink);
