10b91a1abc4e172f45bc09ceed59e344a2919fae
Mathieu Benoit <mathben963@gmail.com>
2017-04-23 21:31:37 -0400

    Fix timeZone and add project_1 - add index.php in project_1 to test webhook from external service

---
+++ project_1/api/calendar/post/index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ Tous les posts : <br/>
+ <?php print_r($_POST); ?>
+ </body>
+</html>
--- project_1/api/calendar/post/quickstart.php
+++ project_1/api/calendar/post/quickstart.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 
 define('APPLICATION_NAME', 'Google Calendar API PHP Quickstart');
-define('CREDENTIALS_PATH', '~/.credentials/calendar-php-quickstart.json');
+define('CREDENTIALS_PATH', '~/.credentials3/calendar-php-quickstart.json');
 define('CLIENT_SECRET_PATH', __DIR__ . '/secret/client_secret.json');
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/calendar-php-quickstart.json
