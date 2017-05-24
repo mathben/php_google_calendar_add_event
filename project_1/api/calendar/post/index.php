d51448277eda3afd611ff5add6eb37a29e66cca9
Mathieu Benoit <mathben963@gmail.com>
2017-05-23 22:22:54 -0400

    index.php : add time support

--- project_1/api/calendar/post/index.php
+++ project_1/api/calendar/post/index.php
@@ -11,11 +11,13 @@ define('SCOPES', implode(' ', array(
   Google_Service_Calendar::CALENDAR)
 ));
 
-  $json_string = json_encode($_POST);
-   $file_handle = fopen(getcwd().'/test.txt', 'w');
-fwrite($file_handle, $json_string);
-fwrite($file_handle, "\n".$_POST["address"]);
-fclose($file_handle);
+//// Begin test
+//$json_string = json_encode($_POST);
+//$file_handle = fopen(getcwd().'/test.txt', 'w');
+//fwrite($file_handle, $json_string);
+//fwrite($file_handle, "\n".$_POST["address"]);
+//fclose($file_handle);
+//// End test
 
 //if (php_sapi_name() != 'cli') {
 //  throw new Exception('This application must be run on the command line.');
@@ -76,70 +78,121 @@ function expandHomeDirectory($path) {
   return str_replace('~', realpath($homeDirectory), $path);
 }
 
-// Get the API client and construct the service object.
-$client = getClient();
-$service = new Google_Service_Calendar($client);
+function main_application() {
+  // Get the API client and construct the service object.
+  $client = getClient();
+  $service = new Google_Service_Calendar($client);
 
-// Print the next 10 events on the user's calendar.
-//$calendarId = 'primary';
-//$optParams = array(
-//  'maxResults' => 10,
-//  'orderBy' => 'startTime',
-//  'singleEvents' => TRUE,
-//  'timeMin' => date('c'),
-//);
-//$results = $service->events->listEvents($calendarId, $optParams);
-//
-//if (count($results->getItems()) == 0) {
-//  print "No upcoming events found.\n";
-//} else {
-//  print "Upcoming events:\n";
-//  foreach ($results->getItems() as $event) {
-//    $start = $event->start->dateTime;
-//    if (empty($start)) {
-//      $start = $event->start->date;
-//    }
-//    printf("%s (%s)\n", $event->getSummary(), $start);
-//  }
-//}
+  // Print the next 10 events on the user's calendar.
+  //$calendarId = 'primary';
+  //$optParams = array(
+  //  'maxResults' => 10,
+  //  'orderBy' => 'startTime',
+  //  'singleEvents' => TRUE,
+  //  'timeMin' => date('c'),
+  //);
+  //$results = $service->events->listEvents($calendarId, $optParams);
+  //
+  //if (count($results->getItems()) == 0) {
+  //  print "No upcoming events found.\n";
+  //} else {
+  //  print "Upcoming events:\n";
+  //  foreach ($results->getItems() as $event) {
+  //    $start = $event->start->dateTime;
+  //    if (empty($start)) {
+  //      $start = $event->start->date;
+  //    }
+  //    printf("%s (%s)\n", $event->getSummary(), $start);
+  //  }
+  //}
 
-// Refer to the PHP quickstart on how to setup the environment:
-// https://developers.google.com/google-apps/calendar/quickstart/php
-// Change the scope to Google_Service_Calendar::CALENDAR and delete any stored
-// credentials.
 
-$event = new Google_Service_Calendar_Event(array(
-  'summary' => $_POST["summary"],
-  'location' => $_POST["address"],
-  'description' => $_POST["firstname"]." ".$_POST["lastname"]."\n".$_POST["description"]."\nEvent auto-generated.",
-  'start' => array(
-    'dateTime' => $_POST["date"].'T'.$_POST["heure_debut_00_00_00"].'-04:00',
-    'timeZone' => 'America/Montreal',
-  ),
-  'end' => array(
-    'dateTime' => $_POST["date"].'T'.$_POST["heure_fin_00_00_00"].'-04:00',
-    'timeZone' => 'America/Montreal',
-  ),
-//  'recurrence' => array(
-//    'RRULE:FREQ=DAILY;COUNT=2'
-//  ),
-//  'attendees' => array(
-//    array('email' => 'lpage@example.com'),
-//    array('email' => 'sbrin@example.com'),
-//  ),
-//  'reminders' => array(
-//    'useDefault' => FALSE,
-//    'overrides' => array(
-//      array('method' => 'email', 'minutes' => 24 * 60),
-//      array('method' => 'popup', 'minutes' => 10),
-//    ),
-//  ),
-));
+  // Refer to the PHP quickstart on how to setup the environment:
+  // https://developers.google.com/google-apps/calendar/quickstart/php
+  // Change the scope to Google_Service_Calendar::CALENDAR and delete any stored
+  // credentials.
 
-$calendarId = 'primary';
-$event = $service->events->insert($calendarId, $event);
-//printf("Event created: %s\n", $event->htmlLink);
+  // Setup heure/minute debut
+  $heure_debut = $_POST["heure_debut"];
+  $minute_debut = $_POST["minute_debut"];
 
+  $temps_debut = $heure_debut.":".$minute_debut.":00";
+
+  // Setup heure/minute fin
+  $heure_debut_nb = (int)$heure_debut;
+  $minute_debut_nb = (int)$minute_debut;
+  $duree_nb = (int)$_POST["duree"];
+
+  $minute_fin_nb = (int)(($minute_debut_nb + $duree_nb) % 60);
+  $heure_fin_nb = (int)($heure_debut_nb + (($minute_debut_nb + $duree_nb) / 60));
+
+  $heure_fin = "";
+  $minute_fin = "";
+
+  if ($heure_fin_nb > 10) {
+    $heure_fin = (string)$heure_fin_nb;
+  } else {
+    $heure_fin = "0".$heure_fin_nb;
+  }
+
+  if ($minute_fin_nb > 10) {
+    $minute_fin = (string)$minute_fin_nb;
+  } else {
+    $minute_fin = "0".$minute_fin_nb;
+  }
+
+  $temps_fin = $heure_fin.":".$minute_fin.":00";
+
+//  // Begin test
+//  $file_handle = fopen(getcwd().'/test3.txt', 'w');
+//  fwrite($file_handle, $temps_fin);
+//  //  fwrite($file_handle, "\n".$_POST["address"]);
+//  fclose($file_handle);
+//  // End test
+
+  // Envoie du message à Google calendar
+  $event = new Google_Service_Calendar_Event(array(
+    'summary' => $_POST["summary"],
+    'location' => $_POST["address"],
+    'description' => $_POST["firstname"]." ".$_POST["lastname"]."\n".$_POST["description"]."\nEvent auto-generated.",
+    'start' => array(
+      'dateTime' => $_POST["date"].'T'.$temps_debut.'-04:00',
+      'timeZone' => 'America/Montreal',
+    ),
+    'end' => array(
+      'dateTime' => $_POST["date"].'T'.$temps_fin.'-04:00',
+      'timeZone' => 'America/Montreal',
+    ),
+  //  'recurrence' => array(
+  //    'RRULE:FREQ=DAILY;COUNT=2'
+  //  ),
+  //  'attendees' => array(
+  //    array('email' => 'lpage@example.com'),
+  //    array('email' => 'sbrin@example.com'),
+  //  ),
+  //  'reminders' => array(
+  //    'useDefault' => FALSE,
+  //    'overrides' => array(
+  //      array('method' => 'email', 'minutes' => 24 * 60),
+  //      array('method' => 'popup', 'minutes' => 10),
+  //    ),
+  //  ),
+  ));
+
+  $calendarId = 'primary';
+  $event = $service->events->insert($calendarId, $event);
+  //printf("Event created: %s\n", $event->htmlLink);
+
+//  // Begin test
+//  $file_handle = fopen(getcwd().'/test2.txt', 'w');
+//  fwrite($file_handle, "Event created: ".$event->htmlLink);
+//  fwrite($file_handle, $temps_debut);
+//  fwrite($file_handle, $temps_fin);
+//  fclose($file_handle);
+//  // End test
+}
+
+main_application();
 ?>
 
 <html>
